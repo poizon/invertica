f037e369be80d67b4cf7c78b980128cfaa41b12c
Paul <poiz@pavel.(none)>
2009-11-07 18:14:29 +0300

    memcached tested 2

--- test.pl
+++ test.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl -w
+#
+#
+$|=1;
+
 use strict;
 
+use CGI qw(header param);
 use Cache::Memcached;
 use DBIx::Abstract;
 
@@ -15,10 +20,10 @@ my $db = DBIx::Abstract->connect({
 
 my  $cache  =  Cache::Memcached->new({servers  => ["127.0.0.1:12345"], debug => 1, compress_threshold => 10000});
 # предположим что поступает запрос на выборку инфо по фильму с id = 3
-my $id = 3;#
+my $id = param('id') || 3;#
 # пробуем получить информацию из кэша согласно $id
 my $movie_info = $cache->get("movie_id_$id");
-
+print header(-charset=>'utf8');
 if(!$movie_info) {
 if ($db->select({fields=>'*',table=>'movie',where =>{'movie_id',['=',$id]} })->rows) {
            while (my $data = $db->fetchrow_arrayref) {
